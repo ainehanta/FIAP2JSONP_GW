380a771963d3e10e8a662700eff0945f3dd33528
Makoto Uju <ainehanta@hotmail.co.jp>
2017-05-11 02:08:27 +0900

    Convert jsonp based gw to json based gw

--- fiap2json.php
+++ fiap2json.php
@@ -1,12 +1,11 @@
 <?php
 /*
- * FIAP2JSONP_GW
- * https://github.com/miettal/FIAP2JSONP_GW
+ * FIAP2JSON_GW
  *
- * Copyright 2012, "miettal" Hiromasa Ihara
+ * Copyright 2017, Makoto Uju
  * Licensed under the MIT license.
+ * Forked from miettal's FIAP2JSONP_GW(https://github.com/miettal/FIAP2JSONP_GW)
  */
-header("Content-Type: text/javascript; charset=utf-8");
 
 function uuid(){
   return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
@@ -16,15 +15,26 @@ function uuid(){
     mt_rand(0, 0xffff), mt_rand(0,0xffff), mt_rand(0,0xffff));
 }
 
-if(!isset($_GET["callback"])){
-  exit();
+function send_response($json_array, $response_code = 200) {
+  ob_start("ob_gzhandler");
+  http_response_code($response_code);
+  header("Content-Type: application/json; charset=utf-8");
+  header("Access-Control-Allow-Origin: *");
+  echo json_encode($json_array);
+  ob_end_flush();
 }
-$callback = $_GET["callback"];
+
+function sanitize_to_array($object) {
+  if( is_array($object) ) {
+    return $object;
+  } else {
+    return array($object);
+  }
+}
 
 if(!isset($_GET["fiap_url"]) ||
    !isset($_GET["key"])){
-  echo "$callback(".json_encode(array("status" => "error",
-    "error_msg" => "fiap_url and key are required.")).")";
+  echo json_encode(array("status" => "error", "error_msg" => "fiap_url and key are required."), 400);
   exit();
 }
 
@@ -32,7 +42,8 @@ $fiap_url = $_GET["fiap_url"];
 $key = $_GET["key"];
 
 try {
-  $fiap = new SoapClient($fiap_url."?wsdl");
+  $fiap = new SoapClient($fiap_url."?wsdl",
+    array('compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP));
 
   $cursor = null;
   $data = array();
@@ -44,29 +55,23 @@ try {
               "cursor" => $cursor,
               "type" => "storage",
               "id" => uuid(),
-              "acceptableSize"=>"5000",
+              "acceptableSize"=>"1000",
               "key" => $key,
             )
           )
         )
       )
     );
-    
+
     if(isset($result->transport->body->point)){
       $point = $result->transport->body->point;
-      if(is_array($point)){
-        $points = $point;
-      }else{
-        $points = array($point);
-      }
+      $points = sanitize_to_array($point);
+
       foreach($points as $point){
         if(isset($point->value)){
           $point_id = $point->id;
-          if(is_array($point->value)){
-            $values = $point->value;
-          }else{
-            $values = array($point->value);
-          }
+          $values = sanitize_to_array($point->value);
+
           foreach($values as $v){
             $value = $v->_;
             $time = $v->time;
@@ -78,17 +83,15 @@ try {
         }
       }
     }
-    
+
     if(isset($result->transport->header->query->cursor)){
       $cursor = $result->transport->header->query->cursor;
     }else{
       $cursor = null;
     }
   }while($cursor !== null);
-} catch (SoapFault $e) {
-  echo "$callback(".json_encode(array("status" => "error",
-    "error_msg" => $e->faultstring)).")";
-  exit();
-}
 
-echo "$callback(".json_encode(array("status" => "success", "data" => $data)).")";
+  send_response(array("status" => "success", "data" => $data));
+} catch (Exception $e) {
+  send_response(array("status" => "error", "error_msg" => $e->faultstring), 400);
+}
